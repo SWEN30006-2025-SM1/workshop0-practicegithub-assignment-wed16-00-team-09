3cfc8bf8316ec705cae4c2ea5b88b4ac182fdfb9
Qi Han Tan <tan.qihan1@gmail.com>
2025-03-05 17:50:42 +1100

    "testing 1"

--- app/src/main/java/example/Main.java
+++ app/src/main/java/example/Main.java
@@ -1,6 +1,6 @@
 package example;
 
-public class Main {
+public class    Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("add = " + calculator.add(3, 5));
